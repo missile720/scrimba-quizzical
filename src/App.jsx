599f3228ead153247887425f9e7d76ac4084f522
Misael Bolainez <misaelbolainez@gmail.com>
2023-05-02 16:23:16 -0700

    feat(init): Initialized React + Vite

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  //Requirements
+  //Two screens (start & questions)
+  //Pull 5 questions from the OTDB API https://opentdb.com/api_config.php
+  //Tally correct answers after "check answers" is clicked
+  //Styled & polished
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
