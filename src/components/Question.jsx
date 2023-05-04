d99a947cdc983727348ffec6df74c4a21767d0e3
Misael <misaelbolainez@gmail.com>
2023-05-04 11:56:09 -0700

    fix(styles): Fixed the rendering for button styles so it is easier to read

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,34 +1,59 @@
 import "./Question.css"
 
 function Question(props) {
-    console.log(props.quizDone)
+  const isQuizDone = props.quizDone;
+  const isCorrect = props.correct;
+  const selectedChoice = props.selected;
 
-    const stylesSelected = {
-        backgroundColor: !props.quizDone ? "#D6DBF5" :  props.correct ? "#94D7A2" : "#F8BCBC",
-        opacity: !props.quizDone ? "1" : props.correct ? "1" : ".5",
-        border: props.quizDone && "none"
-    }
+  const styles = {
+    backgroundColor: isCorrect ? "#94D7A2" : "#F8BCBC",
+    opacity: isQuizDone && isCorrect ? "1" : "0.5"
+  };
 
-    const stylesUnselected = {
-        backgroundColor: "transparent",
-        opacity: props.quizDone ? "0.5" : "1"
-    }
+  const stylesSelected = {
+    ...styles,
+    border: isQuizDone && "none",
+    backgroundColor: !isQuizDone ? "#D6DBF5" : styles.backgroundColor,
+    opacity: !isQuizDone ? "1" : styles.opacity
+  };
 
-    const stylesCorrect = {
-        backgroundColor: "#94D7A2",
-        border: "none"
-    }
+  const stylesUnselected = {
+    ...styles,
+    backgroundColor: "transparent",
+    opacity: isQuizDone ? "0.5" : "1"
+  };
+
+  const stylesCorrect = {
+    backgroundColor: "#94D7A2",
+    border: "none"
+  };
+
+  let buttons = props.choices.map((choice, index) => (
+        <button
+          key={index}
+          className="answerChoice"
+          disabled={isQuizDone}
+          style={
+            choice === selectedChoice
+              ? stylesSelected
+              : choice === props.answer && isQuizDone
+              ? stylesCorrect
+              : stylesUnselected
+          }
+          onClick={() => props.selectButton(props.id, choice)}
+        >
+          {choice}
+        </button>
+      ))
 
   return (
     <div>
-        <h1 className='question'>{props.question}</h1>
-        <button className='answerChoice' disabled={props.quizDone} style={props.choices[0] === props.selected ? stylesSelected : props.correct ? stylesUnselected : props.choices[0] === props.answer && props.quizDone ? stylesCorrect : stylesUnselected} onClick={() => props.selectButton(props.id,props.choices[0])}>{props.choices[0]}</button>
-        <button className='answerChoice' disabled={props.quizDone} style={props.choices[1] === props.selected ? stylesSelected : props.correct ? stylesUnselected : props.choices[1] === props.answer && props.quizDone ? stylesCorrect :stylesUnselected} onClick={() => props.selectButton(props.id,props.choices[1])}>{props.choices[1]}</button>
-        <button className='answerChoice' disabled={props.quizDone} style={props.choices[2] === props.selected ? stylesSelected : props.correct ? stylesUnselected : props.choices[2] === props.answer && props.quizDone ? stylesCorrect :stylesUnselected} onClick={() => props.selectButton(props.id,props.choices[2])}>{props.choices[2]}</button>
-        <button className='answerChoice' disabled={props.quizDone} style={props.choices[3] === props.selected ? stylesSelected : props.correct ? stylesUnselected : props.choices[3] === props.answer && props.quizDone ? stylesCorrect :stylesUnselected} onClick={() => props.selectButton(props.id,props.choices[3])}>{props.choices[3]}</button>
-        <hr className='line'/>
+      <h1 className="question">{props.question}</h1>
+      {buttons}
+      <hr className="line" />
     </div>
-  )
+  );
 }
+
 
 export default Question
